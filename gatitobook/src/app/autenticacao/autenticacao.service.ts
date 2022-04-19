2a96ffb99f76f14f57925da839129ea26bffb917
Marcos Coelho <mvscmarcos@gmail.com>
2022-04-19 15:12:44 -0300

    Aula 4

--- gatitobook/src/app/autenticacao/autenticacao.service.ts
+++ gatitobook/src/app/autenticacao/autenticacao.service.ts
@@ -12,7 +12,7 @@ export class AutenticacaoService {
     private httpClient: HttpClient
   ) { }
 
-  autentica(usuario: string, senha: string): Observable<any> {
+  autenticar(usuario: string, senha: string): Observable<any> {
 
     return this.httpClient.post('http://localhost:3000/user/login', {
       userName: usuario,
